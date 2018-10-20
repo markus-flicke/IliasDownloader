43e8c60d9aabea02008e5e31620b07024f63f99a
m <mark.alex@web.de>
2018-10-20 22:19:09 +0200

    resolved pathing issue

--- Main.py
+++ Main.py
@@ -4,11 +4,11 @@ import os
 
 
 if __name__ == '__main__':
-    if not 'output' in os.listdir(''):
+    if not 'output' in os.listdir('./'):
         os.mkdir('output/')
-    if not 'downloads' in os.listdir(''):
+    if not 'downloads' in os.listdir('./'):
         os.mkdir('downloads/')
-    if not 'credentials.txt' in os.listdir(''):
+    if not 'credentials.txt' in os.listdir('./'):
         with open('credentials.txt', 'w+') as file:
             file.write('username\npassword')
         raise Exception('You need to update username and password in credentials.txt')
